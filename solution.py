3354f181be6a65980213be303e89f6c8ea653408
agariaeff <96379727+agariaeff@users.noreply.github.com>
2023-04-13 19:25:29 +0300

    Create solution.py

--- solution.py
+++ solution.py
@@ -13,5 +13,5 @@ def solution(x_success: int,
     # Не меняйте название функции и её аргументы
     from statsmodels.stats.proportion import proportions_ztest
     stat, pval = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='smaller')
-    alpha = 0.07
+    alpha = 0.02
     return pval < alpha # Ваш ответ, True или False
